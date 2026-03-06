3536458fcb5651c7028508a5c47a8c2665d3ce6b
Mateus Rocha <mateus@Mattbook.local>
2026-03-05 22:25:49 -0500

    joga tudo

--- app/page.js
+++ app/page.js
@@ -2,19 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import { supabase } from "@supabase/supabase-js"
 
-export default async function Home() {
-
-const res = await fetch("https://portal-heymateus.vercel.app/api/readcases", {
-  cache : "no-store",
-});
-
-  const json = await res.json();
-  const data = Array.isArray(json.data) ? json.data : [];
-
-
-  return(
-  <div className="general">
-    
-  </div>
-  );
+export default async function Page() {
+  const data = await fetch('https://portal-heymateus.vercel.app/api/readcases')
+  const cases = await data.json()
+  return (
+    <ul>
+      {cases.map((case) => (
+        <li key={case.id}>{case.title}</li>
+      ))}
+    </ul>
+  )
 }
